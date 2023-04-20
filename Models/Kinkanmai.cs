ca713ea547b24c9c7f9e3e4c07defed6a4e938df
Ppxndpxdd <anawin.pp@gmail.com>
2023-04-20 21:48:30 +0700

    try to add KinKanMaiDb for menu

---
+++ Models/Kinkanmai.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KinKanMai.Models
+{
+    public class Kinkanmai
+    {
+        [Key]
+        public int GroupID { get; set; }
+        public int OrderID { get; set; }
+        [Required]
+        public string SenderName { get; set; }
+        [Required]
+        public string CanteenName { get; set; }
+        [Required]
+        public string FoodName { get; set; }
+        public string Detail { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public int Cost { get; set; }
+        [Required]
+        public string ReceiveName { get; set; }
+        public bool OrderStatus { get; set; }
+    }
+}
